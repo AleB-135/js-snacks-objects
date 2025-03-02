25a716eab788ed71b611749f8cf9c69e7f29483b
AleB-135 <alessiobrusca1999@outlook.com>
2025-03-02 18:38:40 +0100

    feat: Completato JSnack 1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//JSNACK 1
+
+const automobili = [
+{   marca: "Ford", modello: "Mustang", alimentazione: "Benzina"}, 
+{   marca: "BMW", modello: "X3", alimentazione: "Diesel",}, 
+{   marca: "Ferrari", modello: "Spider", alimentazione: "Benzina",},
+{   marca: "Dacia", modello: "Bigster", alimentazione: "GPL",}, 
+{   marca: "Volkswagen", modello: "Polo TGI", alimentazione: "Metano",},
+{   marca: "Cupra", modello: "Tavascan", alimentazione: "Elettrico",},
+{   marca: "DR", modello: "6.0", alimentazione: "GPL",}, 
+{   marca: "Skoda", modello: "Octavia", alimentazione: "Metano",}, 
+{   marca: "Tesla", modello: "Model Y", alimentazione: "Elettrico",},
+{   marca: "Evo", modello: "7", alimentazione: "GPL"}]
+
+const autoBenzina = [];
+const autoDiesel = [];
+
+for (let i = 0; i < automobili.length; i++) {
+    const curAuto = automobili [i];
+    const alimentazioneAuto = curAuto.alimentazione;
+    
+     if (alimentazioneAuto === "Benzina") {
+        autoBenzina.push(curAuto);
+    } else if (alimentazioneAuto === "Diesel") {
+        autoDiesel.push(curAuto);
+    } else {
+      console.log(curAuto);
+    }
+}
+
+console.log(autoBenzina, autoDiesel);
+
+//JSNACK 2
